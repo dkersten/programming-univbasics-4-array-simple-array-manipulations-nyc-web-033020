310b71a400c8b295fa0729461998a35728dee243
Daniel Kersten <dkersten1994@gmail.com>
2020-03-05 14:26:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,17 @@ def pop_with_args(array)
   last_two_elements = array.pop(2)
   last_two_elements
 end
+
+def using_shift(array)
+  first_element = array.shift
+  first_element
+end
+
+def shift_with_args(array)
+  first_two_elements = array.shift(2)
+  first_two_elements
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
